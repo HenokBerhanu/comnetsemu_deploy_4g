fa69c2530523f26ea9416087da2f4c1367c2d10a
Henok Berhanu Tsegaye <henokberhanut@gmail.com>
2023-04-21 12:08:46 +0000

    updates

--- Test_net/E2E.py
+++ Test_net/E2E.py
@@ -9,13 +9,14 @@ from typing import Dict, List
 from comnetsemu.cli import CLI, spawnXtermDocker
 from comnetsemu.net import Containernet, VNFManager
 from comnetsemu.node import DockerHost
-from mininet.link import TCLink, TCIntf, Intf
+from mininet.link import TCLink, TCIntf
 from mininet import log
 from mininet.log import info, setLogLevel
-from mininet.node import Controller, RemoteController, OVSBridge, OVSKernelSwitch
+from mininet.node import Controller, OVSBridge
 from mininet.topo import Topo
 
 root_directory="/home/vagrant/comnetsemu_deploy_4g"
+mongodb_directory="/home/vagrant/mongodbdata"
 
 
 if __name__ == "__main__":
@@ -28,9 +29,44 @@ if __name__ == "__main__":
     env = dict()
     
     net = Containernet(controller=Controller, link=TCLink, waitConnected=True)
+    #net = Containernet(controller=Controller, ipBase="192.168.56.0/24", link=TCLink, waitConnected=True)
 
     cmds: Dict[DockerHost, str] = {}
     hardcoded_ips: List[Dict[str, str]] = []
+
+    info("*** Adding Host for open5gs EPC\n")
+    epc = net.addDockerHost(
+        "epc",
+        dimage="o5gs_epc",
+        ip="192.168.0.10/24",
+        # dcmd="",
+        dcmd = "bash /open5gs/install/etc/open5gs/epc.sh",
+        docker_args={
+            "ports" : { "3000/tcp": 3000 },
+            "volumes": {
+                root_directory + "/epclogs": {
+                    "bind": "/open5gs/install/var/log/open5gs",
+                    "mode": "rw",
+                },
+                mongodb_directory: {
+                    "bind": "/var/lib/mongodb",
+                    "mode": "rw",
+                },
+                root_directory + "/open5gs/config": {
+                    "bind": "/open5gs/install/etc/open5gs",
+                    "mode": "rw",
+                },
+                "/etc/timezone": {
+                    "bind": "/etc/timezone",
+                    "mode": "ro",
+                },
+                "/etc/localtime": {
+                    "bind": "/etc/localtime",
+                    "mode": "ro",
+                },
+            },
+        },
+    )
 
     info("*** Adding Host for SRSRAN ENB\n")
     env["COMPONENT_NAME"]="srsenb"
@@ -140,34 +176,24 @@ if __name__ == "__main__":
 
 
     info("*** Add controller\n")
-    c0 = net.addController('c0', controller=RemoteController, ip='192.168.56.101', port=6633 )
+    net.addController("c0")
 
     info("*** Adding switch\n")
     s1 = net.addSwitch("s1")
     s2 = net.addSwitch("s2")
+    s3 = net.addSwitch("s3")
 
     info("*** Adding links\n")
     net.addLink(s1,  s2, bw=1000, delay="1ms", intfName1="s1-s2",  intfName2="s2-s1")
+    net.addLink(s2,  s3, bw=1000, delay="1ms", intfName1="s2-s3",  intfName2="s3-s2")
+    
     net.addLink(srsue,  s1, bw=1000, delay="1ms", intfName1="srsue-s1",  intfName2="s1-srsue")
     net.addLink(srsenb, s2, bw=1000, delay="1ms", intfName1="srsenb-s2", intfName2="s2-srsenb")
+    net.addLink(epc, s3, bw=1000, delay="1ms", intfName1="epc-s3", intfName2="s3-epc")
     
     info("\n*** Starting network\n")
-    c0.start()
-    s1.start([c0])
-    s2.start([c0])
-    s1.cmd("ip link add s1-gre1 type gretap local 192.168.56.101 remote 192.168.56.102 ttl 64")
-    s1.cmd("ip link set s1-gre1 up")
-    Intf("s1-gre1", node=s1)
-
-    s2.cmd("ip link add s2-gre1 type gretap local 192.168.56.101 remote 192.168.56.102 ttl 64")
-    s2.cmd("ip link set s2-gre1 up")
-    Intf("s2-gre1", node=s2)
-
-    info("\n*** Running CLI\n")
     net.start()
 
 if not AUTOTEST_MODE:
     CLI(net)
-    s1.cmd("ip link del dev s1-gre1")
-    s2.cmd("ip link del dev s2-gre1")
     net.stop()
--- core_o5gs4g.py
+++ core_o5gs4g.py
@@ -1,44 +1,99 @@
-#!/usr/bin/python
+import atexit
+import os
+import shlex
+import signal
+import subprocess
+import time
+from typing import Dict, List
 
-# https://www.google.com/search?q=linux+gre+tunnels+with+mininet&oq=linux+gre+tunnels+with+mininet&aqs=chrome.0.69i59j0i546l2.10984j0j7&sourceid=chrome&ie=UTF-8
-# https://csie1.nqu.edu.tw/smallko/sdn/vm2vm_gre.htm
-# https://techandtrains.com/2014/01/25/using-linux-gre-tunnels-to-connect-two-mininet-networks/
+from comnetsemu.cli import CLI, spawnXtermDocker
+from comnetsemu.net import Containernet, VNFManager
+from comnetsemu.node import DockerHost
+from mininet.link import TCLink, TCIntf, Intf
+from mininet import log
+from mininet.log import info, setLogLevel
+from mininet.node import Controller, RemoteController, OVSBridge, OVSKernelSwitch
+from mininet.topo import Topo
 
-from mininet.net import Mininet
-from mininet.node import  Controller, RemoteController, OVSKernelSwitch
-from mininet.cli import CLI
-from mininet.log import setLogLevel
-from mininet.link import TCLink, Intf
+root_directory="/home/vagrant/comnetsemu_deploy_4g"
+mongodb_directory="/home/vagrant/mongodbdata"
 
-def topology():
 
-    "Create a network."
+if __name__ == "__main__":
 
-    net = Mininet( controller=Controller, link=TCLink, switch=OVSKernelSwitch )
-    print("*** Creating nodes")
-    h2 = net.addHost( 'h2', ip="10.0.0.2" )
-    s2 = net.addSwitch( 's2')
+    AUTOTEST_MODE = os.environ.get("COMNETSEMU_AUTOTEST_MODE", 0)
+
+    setLogLevel("info")
+    
+
+    env = dict()
+    
+    net = Containernet(controller=Controller, link=TCLink, waitConnected=True)
+
+
+    info("*** Adding Host for open5gs EPC\n")
+    epc = net.addDockerHost(
+        "epc",
+        dimage="o5gs_epc",
+        ip="192.168.0.10/24",
+        # dcmd="",
+        dcmd = "bash /open5gs/install/etc/open5gs/epc.sh",
+        docker_args={
+            "ports" : { "3000/tcp": 3000 },
+            "volumes": {
+                root_directory + "/epclogs": {
+                    "bind": "/open5gs/install/var/log/open5gs",
+                    "mode": "rw",
+                },
+                mongodb_directory: {
+                    "bind": "/var/lib/mongodb",
+                    "mode": "rw",
+                },
+                root_directory + "/open5gs/config": {
+                    "bind": "/open5gs/install/etc/open5gs",
+                    "mode": "rw",
+                },
+                "/etc/timezone": {
+                    "bind": "/etc/timezone",
+                    "mode": "ro",
+                },
+                "/etc/localtime": {
+                    "bind": "/etc/localtime",
+                    "mode": "ro",
+                },
+            },
+        },
+    )
+
+    info("*** Add controller\n")
     c0 = net.addController('c0', controller=RemoteController, ip='192.168.56.101', port=6633 )
 
-    print("*** Adding Link")
-    net.addLink(h2,s2)
+    info("*** Adding switch\n")
+    s1 = net.addSwitch("s1")
+    s2 = net.addSwitch("s2")
 
-    print("*** Starting network")
+    info("*** Adding links\n")
+    net.addLink(s1,  s2, bw=1000, delay="1ms", intfName1="s1-s2",  intfName2="s2-s1")
+    net.addLink(srsue,  s1, bw=1000, delay="1ms", intfName1="srsue-s1",  intfName2="s1-srsue")
+    net.addLink(srsenb, s2, bw=1000, delay="1ms", intfName1="srsenb-s2", intfName2="s2-srsenb")
+    
+    info("\n*** Starting network\n")
     c0.start()
+    s1.start([c0])
     s2.start([c0])
-    s2.cmd("ip link add s2-gre1 type gretap local 192.168.56.102 remote 192.168.56.101 ttl 64")
+    s1.cmd("ip link add s1-gre1 type gretap local 192.168.56.101 remote 192.168.56.102 ttl 64")
+    s1.cmd("ip link set s1-gre1 up")
+    Intf("s1-gre1", node=s1)
+
+    s2.cmd("ip link add s2-gre1 type gretap local 192.168.56.101 remote 192.168.56.102 ttl 64")
     s2.cmd("ip link set s2-gre1 up")
     Intf("s2-gre1", node=s2)
 
-
-    print("*** Running CLI")
+    info("\n*** Running CLI\n")
     net.start()
-    CLI( net )
 
-    print("*** Stopping network")
+if not AUTOTEST_MODE:
+    CLI(net)
+    s1.cmd("ip link del dev s1-gre1")
     s2.cmd("ip link del dev s2-gre1")
     net.stop()
-
-if __name__ == '__main__':
-    setLogLevel( 'info' )
-    topology()
